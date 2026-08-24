d9c70d49b5f60e06065560dbc60acb5d291bf0df
Sufyan Naeem <179460611+SUFI-410@users.noreply.github.com>
2026-08-24 22:13:00 +0500

    Fix authenticated workspace page scrolling

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,11 +1,11 @@
-﻿import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
 function AppLayout() {
   return (
-    <div className="flex min-h-screen flex-col bg-background">
+    <div className="flex h-dvh min-h-0 flex-col overflow-hidden bg-background">
       <Header />
 
       <div className="flex min-h-0 flex-1 flex-col md:flex-row">
